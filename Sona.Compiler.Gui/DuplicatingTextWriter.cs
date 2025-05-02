9f0fe5c11368768f4f0524557a5f7be2acd69bf5
IS4 <illidans4@gmail.com>
2025-05-02 22:58:23 +0200

    missing file

---
+++ Sona.Compiler.Gui/DuplicatingTextWriter.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Sona.Compiler.Gui
+{
+    sealed class DuplicatingTextWriter : TextWriter
+    {
+        readonly TextWriter first, second;
+
+        public override Encoding Encoding => first.Encoding;
+
+        public DuplicatingTextWriter(TextWriter first, TextWriter second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public override void Write(char value)
+        {
+            first.Write(value);
+            second.Write(value);
+        }
+
+        public override void Write(char[]? buffer)
+        {
+            first.Write(buffer);
+            second.Write(buffer);
+        }
+
+        public override void Write(string? value)
+        {
+            first.Write(value);
+            second.Write(value);
+        }
+
+        public override void Write(char[] buffer, int index, int count)
+        {
+            first.Write(buffer, index, count);
+            second.Write(buffer, index, count);
+        }
+
+        public override void WriteLine()
+        {
+            first.WriteLine();
+            second.WriteLine();
+        }
+    }
+}
