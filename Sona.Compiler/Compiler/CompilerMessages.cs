97537fc4057b9cc14c168895fa6e9672a498087b
IS4 <illidans4@gmail.com>
2025-10-03 16:43:57 +0200

    a few more message rewrites

--- Sona.Compiler/Compiler/CompilerMessages.cs
+++ Sona.Compiler/Compiler/CompilerMessages.cs
@@ -64,6 +64,9 @@ namespace Sona.Compiler
             // No hidden exceptions
             25, // Incomplete pattern matches on this expression.
 
+            // No hidden exceptions
+            40, // This and other recursive references to the object(s) being defined will be checked for initialization-soundness at runtime through the use of a delayed reference. This is because you are defining one or more recursive objects, rather than recursive functions. This warning may be suppressed by using '#nowarn "40"' or '--nowarn:40'.
+
             // Pattern variables must be lowercase
             49, // Uppercase variable identifiers should not generally be used in patterns, and may indicate a missing open declaration or a misspelt pattern name.
             
@@ -83,11 +86,26 @@ namespace Sona.Compiler
 
         static readonly MessageReplacementDictionary messageReplacement = new()
         {
+            // Simplify wording and remove F#-specific parts
+            { 40, "LetRecCheckedAtRuntime", "This value depends on variables declared later in the code whose value may not yet be available. Consider using functions instead of variables." }, // This and other recursive references to the object(s) being defined will be checked for initialization-soundness at runtime through the use of a delayed reference. This is because you are defining one or more recursive objects, rather than recursive functions. This warning may be suppressed by using '#nowarn "40"' or '--nowarn:40'.
+
             // Ignore for anonymous types
             { 64, "NonRigidTypar1", m => null }, // This construct causes code to be less generic than indicated by its type annotations. The type variable implied by the use of a '#', '_' or other type annotation at or near '%s' has been constrained to be type '%s'.
 
+            // Different mechanism
+            { 821, "tcBindingCannotBeUseAndRec", "A 'use' declaration is not permitted together with '#pragma recursive'." }, // A binding cannot be marked both 'use' and 'rec'
+            
+            // Different mechanism
+            { 874, "tcOnlyRecordFieldsAndSimpleLetCanBeMutable", "A 'var' declaration is not permitted together with '#pragma recursive' or '#pragma forwardref'." }, // Mutable 'let' bindings can't be recursive or defined in recursive modules or namespaces
+
             // Compiler-generated unused variables are reported even when they start on _.
-            { 1182, "chkUnusedValue", m => Syntax.GetIdentifierValue(m.Groups[1].Value).StartsWith("_") ? null : m.Value }, // The value '%s' is unused
+            { 1182, "chkUnusedValue", m => Syntax.GetIdentifierValue(m.Groups[1].Value).StartsWith("_", StringComparison.Ordinal) ? null : m.Value }, // The value '%s' is unused
+
+            // Not a warning anymore
+            { 3517, "optFailedToInlineSuggestedValue", "The function parameter is declared as 'inline' but the provided argument cannot be inlined. Consider passing an inlineable anonymous function." }, // The value '%s' was marked 'InlineIfLambda' but was not determined to have a lambda value.
+
+            // Syntactically valid
+            { 3521, "tcInvalidMemberDeclNameMissingOrHasParen", "A complex pattern is not permitted in this declaration." }, // Invalid member declaration. The name of the member is missing or has parentheses.
         };
 
         public static string? ProcessDiagnostic(int code, string message)
@@ -98,47 +116,67 @@ namespace Sona.Compiler
             }
 
             // Find actually replaced message
-            foreach(var (pattern, replacement) in list)
+            foreach(var (pattern, replacer) in list)
             {
-                bool replaced = false;
-                bool ignored = false;
-
-                message = pattern.Replace(message, m => {
-                    var result = replacement(m);
-                    if(result == null)
-                    {
-                        // Null result - ignore message completely
-                        ignored = true;
-                        return "";
-                    }
-                    if(result != m.Value)
-                    {
-                        replaced = true;
-                    }
-                    return result;
-                });
-
-                if(ignored)
+                switch(replacer)
                 {
-                    return null;
-                }
-                if(replaced)
-                {
-                    return message;
+                    case string str:
+                        // Simple replacement, always succeeds
+                        return pattern.Replace(message, str);
+
+                    case Func<Match, string?> func:
+                        // Replacement function
+                        bool replaced = false;
+                        bool ignored = false;
+
+                        message = pattern.Replace(message, m => {
+                            var result = func(m);
+                            if(result == null)
+                            {
+                                // Null result - ignore message completely
+                                ignored = true;
+                                return "";
+                            }
+                            if(result != m.Value)
+                            {
+                                replaced = true;
+                            }
+                            return result;
+                        });
+
+                        if(ignored)
+                        {
+                            return null;
+                        }
+                        if(replaced)
+                        {
+                            return message;
+                        }
+                        break;
                 }
             }
             return message;
         }
 
-        sealed class MessageReplacementDictionary : Dictionary<int, List<(Regex regex, Func<Match, string?>)>>
+        sealed class MessageReplacementDictionary : Dictionary<int, List<(Regex regex, object replacer)>>
         {
-            public void Add(int code, string key, Func<Match, string?> replacement)
+            private void Add(int code, string key, object replacer)
             {
                 if(!this.TryGetValue(code, out var list))
                 {
                     this[code] = list = new();
                 }
-                list.Add((Resources.GetRegex(key, Culture), replacement));
+                list.Add((Resources.GetRegex(key, Culture), replacer));
+            }
+
+            public void Add(int code, string key, Func<Match, string?> replacer)
+            {
+                Add(code, key, (object)replacer);
+            }
+
+            public void Add(int code, string key, string replacer)
+            {
+                Add(code, key, (object)replacer);
             }
         }
     }
